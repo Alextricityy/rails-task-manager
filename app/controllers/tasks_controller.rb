c748ab0563a931a566c13ca3e2220cac16bbb77f
Alex Sell <alextricityy@gmail.com>
2018-05-15 15:16:08 +0100

    logic done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,27 +3,51 @@ class TasksController < ApplicationController
     @tasks = Task.all
 
   end
+
   def show
-    findtask
+    @task = Task.find(params[:id])
   end
+
   def new
+    @task = Task.new
 
   end
+
   def create
+    @task = Task.new(params_task)
+    @task.save
+    redirect_to tasks_path
 
   end
+
+  def edit
+     @task = Task.find(params[:id])
+
+
+  end
+
   def update
+    @task = Task.find(params[:id])
+    @task.update(params_task)
+    redirect_to tasks_path
 
   end
+
   def destroy
+     @task = Task.find(params[:id])
+     @task.destroy
+     redirect_to tasks_path
 
   end
-
 
   private
 
   def findtask
     @task = Task.find(params[:id])
+  end
+
+  def params_task
+    params.require(:task).permit(:title,:details,:completed)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,17 @@ Rails.application.routes.draw do
   get "tasks", to: "tasks#index"
 
   # red
-  get "tasks/:id", to: "tasks#show"
+  get "tasks/new", to: "tasks#new"
 
   # new
-  get "tasks/new", to: "tasks#new"
   # create
-  post "tasks", to: "tasks#save"
+  post "tasks", to: "tasks#create"
   # update
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  get "tasks/:id", to: "tasks#show", as: :task
 
   # delete
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy_task
 end
